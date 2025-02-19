3669bca9472383b9267b02d08baadac45eaf0a37
SUnnnnn111111111111111 <oleg.grishaev02@mail.ru>
2025-02-19 10:46:31 +0300

    Передвижение фигуры

--- Assets/Scripts/HandlerForClickingAMouseButtonOnAnObject.cs
+++ Assets/Scripts/HandlerForClickingAMouseButtonOnAnObject.cs
@@ -3,10 +3,13 @@ using UltEvents;
 
 public class HandlerForClickingAMouseButtonOnAnObject : MonoBehaviour
 {
+    public bool isActive;
     [SerializeField] private UltEvent  WhenClickOnAnObject;
+
 
     void OnMouseDown()
     {
+        if(isActive)
         WhenClickOnAnObject.Invoke();
     }
 }
