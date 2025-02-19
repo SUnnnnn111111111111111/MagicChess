f05a70c8a37a3176e2add0be0109f68279e24d4b
SUnnnnn111111111111111 <oleg.grishaev02@mail.ru>
2025-02-19 14:44:08 +0300

    Перед установкой "статик" MessageSender

--- Assets/Scripts/ScriptableObjectsScript/GlobalGameObjectStorage.cs
+++ Assets/Scripts/ScriptableObjectsScript/GlobalGameObjectStorage.cs
@@ -1,7 +1,22 @@
 using UnityEngine;
+using UltEvents;
 
 [CreateAssetMenu(fileName = "GlobalGameObjectStorage", menuName = "ScriptableObjects/GlobalGameObjectStorage", order = 1)]
 public class GlobalGameObjectStorage : ScriptableObject
 {
-    [field:SerializeField] public GameObject GameObject{ get; set; } 
+    [SerializeField] GameObject gameObject;
+    [SerializeField] private UltEvent<GameObject> BeforeSet;
+    [SerializeField] private UltEvent<GameObject> AfterSet;
+
+    public GameObject GameObject
+    {
+        get => gameObject;
+
+        set 
+        {
+            BeforeSet.Invoke(gameObject);
+            gameObject = value;
+            AfterSet.Invoke(gameObject);
+        }
+    }
 }
